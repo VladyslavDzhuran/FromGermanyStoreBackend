056bbe7dcf8b216b7a0652d9585eb4efe1c3c532
DESKTOP-OHLAKGG\1 <vlad.94dd@gmail.com>
2023-06-21 15:45:13 +0300

    Add application context and Product model

---
+++ FromGermanyStoreDataAccessLibrary/DataAccess/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using FromGermanyStoreDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromGermanyStoreDataAccessLibrary.DataAccess
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
